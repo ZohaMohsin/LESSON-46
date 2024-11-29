70b1454eff721f05a983a0eb043e4eed2da42fb3
ZohaMohsin <amnamohsin72@gmail.com>
2024-11-29 21:28:33 +0500

    act 2

--- act 2.py
+++ act 2.py
@@ -8,3 +8,16 @@ class libraRY:
         print("we have following books")
         for books in self.booklist:
             print(books)
+
+    def lendbook(self,user,book):
+        if book not in self.bookdict:
+         self.bookdict.update({book:user})
+         print("you can take the book")
+        else:
+          print("this is already lended to ",self.bookdict[book])
+    def addbook(self,book):
+        self.booklist.append(book)
+        print("book has been added")
+        
+    def returnbook(self,book):
+        self.bookdict.pop(book)
